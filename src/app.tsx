6017a4f72a012bddc192bb6a698773dcdcd341f0
Harshit Gupta <harshit.gupta@appinventiv.com>
2022-06-03 23:13:44 +0530

    public route add

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Hello from '@components/Hello';
 import ROUTE_NAME from '@utils/route-name';
+import PublicRoute from './template/public.route';
 
 function App() {
   return (
     <Routes>
-      <Route path={ROUTE_NAME.HOME} element={<Hello />}></Route>
+      <Route
+        path={ROUTE_NAME.HOME}
+        element={
+          <PublicRoute>
+            <Hello />
+          </PublicRoute>
+        }
+      ></Route>
     </Routes>
   );
 }
--- src/template/public.route.tsx
+++ src/template/public.route.tsx
@@ -1,11 +1,19 @@
 import Box from '@mui/material/Box';
+import Header from '@components/header/header';
+import Footer from '@components/footer/footer';
 
 type PublicRouteProps = {
   children: React.ReactNode;
 };
 
 function PublicRoute({ children }: PublicRouteProps) {
-  return <Box>{children}</Box>;
+  return (
+    <Box>
+      <Header />
+      {children}
+      <Footer />
+    </Box>
+  );
 }
 
 export default PublicRoute;
